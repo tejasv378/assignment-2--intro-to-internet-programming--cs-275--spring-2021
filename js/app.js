bd8102d406a20eac526cfd5cebcdc03bc8024fff
tejasv378 <venkatesw@hartford.edu>
2021-05-02 21:44:29 -0400

    Added /.. to link and script tags for HTML to recognize style.css and app.js files

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-let riceCups = parseInt(prompt(`Enter number of cups of rice`));
+let riceCups = parseFloat(prompt(`Enter number of cups of rice`));
 let sproutedRice = document.getElementById(`Sprouted`);
 let whiteRice = document.getElementById(`White`);
 
@@ -8,11 +8,11 @@ function sproutedRecipe() {
     let sproutedWaterCups = riceCups * 1.6;
     sproutedRice.innerHTML =
 	`For slightly al dente rice: `+
-	`Combine ` + riceCups.toFixed(0) + ` cups of rice with ` + String(sproutedWaterCups) +
-    ` cups of water or broth and 1 Tbsp olive oil.` +
+	`Combine ` + riceCups + ` cups of rice with ` + String(sproutedWaterCups) +
+    ` cups of water or broth and 1 Tbsp olive oil. ` +
     `Bring to a boil and stir once to mix. ` +
     `Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes. ` +
-    `Remove from heat and let stand for 5 minutes. Fluff with a fork and serve.<br>` +
+    `Remove from heat and let stand for 5 minutes. Fluff with a fork and serve.<br><br>` +
     `For softer rice: ` +
     `Increase liquid by 1/2 cup and cook time by 5 minutes.`;
     whiteRice.innerHTML = ``;
@@ -22,7 +22,7 @@ whiteRecipe();
 function whiteRecipe() {
     let whiteWaterCups = riceCups * 2.0;
     whiteRice.innerHTML =
-    `Combine ` +  riceCups.toFixed(0) + ` cups of rice with ` + String(whiteWaterCups) +
+    `Combine ` +  riceCups + ` cups of rice with ` + String(whiteWaterCups) +
     ` cups of water and 1 Tbsp olive oil.` +
     ` Bring to a boil, then reduce heat to the lowest setting.` +
     ` Cook for about 18 minutes.`;
